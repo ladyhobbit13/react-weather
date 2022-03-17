8a090796a3fc5546e0dc1d4c11a0e555d977639c
ladyhobbit13 <ladyhobbit13@gmail.com>
2022-03-17 00:09:40 -0700

    Added Footer
    Open source link and personal page link

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
 import React from "react";
 import "./App.css";
 
-function App() {
-  return <div className="App">Walking Weather</div>;
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Walking Weather</h1>
+      <footer>
+        This project was coded by{" "}
+        <a href="https://focused-borg-61dda1.netlify.app/" target="_blank">
+          Claire Fleischer
+        </a>{" "}
+        and is{" "}
+        <a href="https://github.com/ladyhobbit13/react-weather" target="_blank">
+          open-sourced on GitHub.
+        </a>
+      </footer>
+    </div>
+  );
 }
-
-export default App;
