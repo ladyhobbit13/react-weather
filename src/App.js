072f0a128aa36dce940c1b18aa01bad70cb37d2b
ladyhobbit13 <ladyhobbit13@gmail.com>
2022-03-18 14:58:23 -0700

    Added Bootstrap

--- src/App.js
+++ src/App.js
@@ -4,17 +4,22 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Walking Weather</h1>
-      <footer>
-        This project was coded by{" "}
-        <a href="https://focused-borg-61dda1.netlify.app/" target="_blank">
-          Claire Fleischer
-        </a>{" "}
-        and is{" "}
-        <a href="https://github.com/ladyhobbit13/react-weather" target="_blank">
-          open-sourced on GitHub.
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Walking Weather</h1>
+        <footer>
+          This project was coded by{" "}
+          <a href="https://focused-borg-61dda1.netlify.app/" target="_blank">
+            Claire Fleischer
+          </a>{" "}
+          and is{" "}
+          <a
+            href="https://github.com/ladyhobbit13/react-weather"
+            target="_blank"
+          >
+            open-sourced on GitHub.
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
