686c582ecf92a57570bbda46c0b33560ed58e195
ladyhobbit13 <ladyhobbit13@gmail.com>
2022-03-19 01:16:31 -0700

    Fixed Warnings

--- src/App.js
+++ src/App.js
@@ -9,13 +9,18 @@ export default function App() {
         <Weather />
         <footer>
           This project was coded by{" "}
-          <a href="https://focused-borg-61dda1.netlify.app/" target="_blank">
+          <a
+            href="https://focused-borg-61dda1.netlify.app/"
+            target="_blank"
+            rel="noreferrer"
+          >
             Claire Fleischer
           </a>{" "}
           and is{" "}
           <a
             href="https://github.com/ladyhobbit13/react-weather"
             target="_blank"
+            rel="noreferrer"
           >
             open-sourced on GitHub.
           </a>
