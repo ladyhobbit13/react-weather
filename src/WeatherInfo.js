099bd9bdc4ad182dcfab7bebb68991bfd5cb2e9d
ladyhobbit13 <ladyhobbit13@gmail.com>
2022-03-21 10:52:26 -0700

    Changed Clearfix to d-flex

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -13,13 +13,9 @@ export default function WeatherInfo(props) {
       </ul>
       <div className="row mt-3">
         <div className="col-6">
-          <div className="clearfix">
+          <div className="d-flex">
             <div className="float-left">
-              <img
-                src={props.data.iconUrl}
-                alt={props.data.description}
-                className="mb-4"
-              />
+              <img src={props.data.iconUrl} alt={props.data.description} />
               <span className="temperature">
                 {Math.round(props.data.temperature)}
               </span>
