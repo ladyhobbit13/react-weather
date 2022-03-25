e398079f540af577a21823d436404120b8d7928d
ladyhobbit13 <ladyhobbit13@gmail.com>
2022-03-25 15:50:02 -0700

    Added Forecast API

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,8 +1,19 @@
 import React from "react";
 import WeatherIcon from "./WeatherIcon";
 import "./WeatherForecast.css";
+import axios from "axios";
 
-export default function WeatherForecast() {
+export default function WeatherForecast(props) {
+  function handleResponse(response) {
+    console.log(response.data);
+  }
+  let apiKey = "1311cf318d3edfa90a5746a7bee262dc";
+  let longitude = props.coordinates.lon;
+  let latitude = props.coordinates.lat;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}`;
+
+  axios.get(apiUrl).then(handleResponse);
+
   return (
     <div className="WeatherForecast">
       <div className="row">
